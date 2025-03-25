f2d29c8c0535fd05007e3895d1e1b074be809b03
Marcos Gil <marcosgildeoliveira@gmail.com>
2025-03-25 11:51:34 +0100

    made more plots

--- density_mat_plot.jl
+++ density_mat_plot.jl
@@ -2,26 +2,33 @@ using CairoMakie, GeometryBasics, HDF5, LinearAlgebra, QuantumMeasurements
 
 mode_name = "PHI(+)+Vv"
 
-ρ = h5open("Results/density_mats_projected.h5") do file
-    read(file["$mode_name/prediction"])
+mode_names = h5open("Results/density_mats_projected.h5") do file
+    keys(file)
 end
 
-with_theme(theme_latexfonts()) do
-    fig = Figure(;fontsize=20)
-    ax = Axis3(fig[1, 1],
-        xticks=(1:4, [L"| H, H \rangle", L"| H, V \rangle", L"| V, H \rangle", L"| V, V \rangle"]),
-        yticks=(1:4, [L"| H, H \rangle", L"| H, V \rangle", L"| V, H \rangle", L"| V, V \rangle"]),
-        xlabelvisible=false,
-        ylabelvisible=false,
-        zlabel=L"\Re(\rho)",
-    )
+for mode_name ∈ mode_names
+    ρ = h5open("Results/density_mats_projected.h5") do file
+        read(file["$mode_name/prediction"])
+    end
+    
+    with_theme(theme_latexfonts()) do
+        fig = Figure(;fontsize=20)
+        ax = Axis3(fig[1, 1],
+            xticks=(1:4, [L"| H, H \rangle", L"| H, V \rangle", L"| V, H \rangle", L"| V, V \rangle"]),
+            yticks=(1:4, [L"| H, H \rangle", L"| H, V \rangle", L"| V, H \rangle", L"| V, V \rangle"]),
+            xlabelvisible=false,
+            ylabelvisible=false,
+            zlabel=L"\Im(\rho)",
+        )
+    
+        data = imag(ρ)
+        meshscatter!(
+            ax, axes(data, 1), axes(data, 2), fill(0, size(data)),
+            marker=Rect3D(Vec3f0(-0.8, -0.8, 0), Vec3f0(1)),
+            markersize=Vec3f0.(0.8, 0.8, vec(data)),
+        )
+        save("Results/MatPlots/imag_mat_$mode_name.pdf", fig)
 
-    data = real(ρ)
-    meshscatter!(
-        ax, axes(data, 1), axes(data, 2), fill(0, size(data)),
-        marker=Rect3D(Vec3f0(-0.8, -0.8, 0), Vec3f0(1)),
-        markersize=Vec3f0.(0.8, 0.8, vec(data)),
-    )
-    save("Results/mat_$mode_name.pdf", fig)
-    fig
+        fig
+    end
 end
